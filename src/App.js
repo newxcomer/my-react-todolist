cb79c1614925fd2ed194f16c12deee287375d7e8
Binh Nguyen <binh.nguyen@carpoolagency.com>
2017-10-29 14:57:03 +0700

    Disable buttons function

--- src/App.js
+++ src/App.js
@@ -6,12 +6,31 @@ import TodoList from './components/todoList';
 import actions from './actions/';
 import './App.css';
 
-export const App = ({ submitTodo, todos, deleteTodo, undeleteTodo }) => (
-  <div>
-    <h1>Todo list</h1>
-    <AddTodo submitTodo={submitTodo} undeleteTodo={undeleteTodo} />
-    <TodoList todos={todos} deleteTodo={deleteTodo} />
-  </div>
+export const App = ({
+    submitTodo,
+    todos,
+    deleteTodo,
+    undeleteTodo,
+    inputChanged,
+    disableAddTodo,
+    disableUndelete,
+  }) => (
+    <div>
+      <h1>Todo list</h1>
+
+      <AddTodo
+        submitTodo={submitTodo}
+        undeleteTodo={undeleteTodo}
+        inputChanged={inputChanged}
+        disableAddTodo={disableAddTodo}
+        disableUndelete={disableUndelete}
+      />
+
+      <TodoList
+        todos={todos}
+        deleteTodo={deleteTodo}
+      />
+    </div>
 );
 
 App.propTypes = {
@@ -24,6 +43,9 @@ App.propTypes = {
   )).isRequired,
   deleteTodo: PropTypes.func.isRequired,
   undeleteTodo: PropTypes.func.isRequired,
+  inputChanged: PropTypes.func.isRequired,
+  disableAddTodo: PropTypes.bool.isRequired,
+  disableUndelete: PropTypes.bool.isRequired,
 };
 
 const mapStateToProps = state => state.todoListApp;
@@ -41,6 +63,10 @@ const mapDispatchToProps = dispatch => ({
 
   undeleteTodo: () => {
     dispatch(actions.undeleteTodo());
+  },
+
+  inputChanged: (text) => {
+    dispatch(actions.inputChanged(text));
   },
 });
 
