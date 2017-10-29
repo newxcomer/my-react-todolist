d24801b208eb8e0dbefc638b90100f5f33b9ffae
Binh Nguyen <binh.nguyen@carpoolagency.com>
2017-10-29 11:46:46 +0700

    Write feature test

--- e2etests/test.js
+++ e2etests/test.js
@@ -1,10 +1,22 @@
+/* global describe, it, browser */
+
 const expect = require('chai').expect;
 
 describe('TodoList App', () => {
-  it('Should load with the right title', () => {
-    browser.url('http://localhost:3000/');
-    const actualTitle = browser.getTitle();
+    it('Should load with the right title', () => {
+        browser.url('http://localhost:3000/');
+        const actualTitle = browser.getTitle();
 
-    expect(actualTitle).to.eql('Todo List');
-  });
+        expect(actualTitle).to.eql('Todo List');
+    });
+
+    it('Should allow me to create a Todo', () => {
+        const todoText = 'Get better at testing';
+        browser.url('http://localhost:3000/');
+        browser.element('.todo-input').setValue(todoText);
+        browser.click('.todo-submit');
+        const actual = browser.element('.todo-text').getText();
+
+        expect(actual).to.equal(todoText);
+    });
 });
